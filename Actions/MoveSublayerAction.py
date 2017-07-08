f95a16fa104da0ad0269ec5f46c811cfaeeebd1b
cmann1 <thecmann1@gmail.com>
2017-07-08 15:41:20 +0200

    Added bounnds option to move props action

--- Actions/MoveSublayerAction.py
+++ Actions/MoveSublayerAction.py
@@ -20,6 +20,7 @@ class MoveSublayerAction(Action):
 		self.vars['rotation'] = (tk.DoubleVar(), 0)
 		self.vars['repeat'] = (tk.IntVar(), 1)
 		self.vars['copy'] = (tk.BooleanVar(), False)
+		self.vars['withinPlayerBounds'] = (tk.BooleanVar(), False)
 
 		self.message_var = tk.StringVar()
 	# END __init__
@@ -39,14 +40,19 @@ class MoveSublayerAction(Action):
 
 		layer_frame = ttk.Frame(dlg, padding=PADDING)
 		layer_frame.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.W, tk.E))
+		copy_frame = ttk.Labelframe(dlg, text='Copy', padding=PADDING)
+		copy_frame.grid(column=0, row=1, sticky=(tk.N, tk.S, tk.W, tk.E), padx=PADDING)
 		options_frame = ttk.Frame(dlg, padding=PADDING)
-		options_frame.grid(column=0, row=1, sticky=(tk.N, tk.S, tk.W, tk.E))
+		options_frame.grid(column=0, row=2, sticky=(tk.N, tk.S, tk.W, tk.E))
 		button_frame = ttk.Frame(dlg, padding=PADDING)
-		button_frame.grid(column=0, row=2, sticky=(tk.N, tk.S, tk.W, tk.E))
+		button_frame.grid(column=0, row=3, sticky=(tk.N, tk.S, tk.W, tk.E))
+
+		layer_frame.columnconfigure(0, weight=1)
+		layer_frame.columnconfigure(1, weight=1)
 
 		from_frame = ttk.Labelframe(layer_frame, text='From', padding=PADDING)
-		to_frame = ttk.Labelframe(layer_frame, text='To', padding=PADDING)
 		from_frame.grid(column=0, row=0, sticky=(tk.N, tk.S, tk.W, tk.E), padx=(0, PADDING))
+		to_frame = ttk.Labelframe(layer_frame, text='To', padding=PADDING)
 		to_frame.grid(column=1, row=0, sticky=(tk.N, tk.S, tk.W, tk.E))
 
 		ttk.Label(from_frame, text='Layer').grid(column=0, row=0, sticky=tk.W)
@@ -58,17 +64,24 @@ class MoveSublayerAction(Action):
 		tk.Spinbox(from_frame, from_=-1, to=24, textvariable=self.vars['sublayer1'][0]).grid(column=0, row=3)
 		tk.Spinbox(to_frame, from_=-1, to=24, textvariable=self.vars['sublayer2'][0]).grid(column=0, row=3)
 
-		ttk.Label(options_frame, text='Offset').grid(column=0, row=0, sticky=tk.W)
-		tk.Spinbox(options_frame, from_=offset_min, to=offset_max, increment=0.1, textvariable=self.vars['offsetX'][0]).grid(column=1, row=0)
-		tk.Spinbox(options_frame, from_=offset_min, to=offset_max, increment=0.1, textvariable=self.vars['offsetY'][0]).grid(column=2, row=0, padx=(PADDING, 0))
+		ttk.Checkbutton(copy_frame, text='Copy', variable=self.vars['copy'][0], onvalue=True,
+						offvalue=False).grid(column=0, row=0, sticky=tk.W)
 
-		ttk.Label(options_frame, text='Rotation').grid(column=0, row=1, sticky=tk.W, pady=PADDING)
-		tk.Spinbox(options_frame, from_=-360, to=360, textvariable=self.vars['rotation'][0]).grid(column=1, row=1)
+		ttk.Label(copy_frame, text='Offset').grid(column=0, row=1, sticky=tk.W)
+		tk.Spinbox(copy_frame, from_=offset_min, to=offset_max, increment=0.1, textvariable=self.vars['offsetX'][0]).grid(column=1, row=0)
+		tk.Spinbox(copy_frame, from_=offset_min, to=offset_max, increment=0.1, textvariable=self.vars['offsetY'][0]).grid(column=2, row=0, padx=(PADDING, 0))
 
-		ttk.Label(options_frame, text='Repeat').grid(column=0, row=2, sticky=tk.W, pady=PADDING)
-		tk.Spinbox(options_frame, from_=1, to=500, textvariable=self.vars['repeat'][0]).grid(column=1, row=2)
-		ttk.Checkbutton(options_frame, text='Copy', variable=self.vars['copy'][0], onvalue=True,
-		                       offvalue=False).grid(column=0, row=3, sticky=tk.W)
+		ttk.Label(copy_frame, text='Rotation').grid(column=0, row=2, sticky=tk.W, pady=PADDING)
+		tk.Spinbox(copy_frame, from_=-360, to=360, textvariable=self.vars['rotation'][0]).grid(column=1, row=1)
+
+		ttk.Label(copy_frame, text='Repeat').grid(column=0, row=3, sticky=tk.W, pady=PADDING)
+		tk.Spinbox(copy_frame, from_=1, to=500, textvariable=self.vars['repeat'][0]).grid(column=1, row=2)
+
+		selective_checkbox = ttk.Checkbutton(options_frame, text='Within player 3 and 4 bounds',
+			variable=self.vars['withinPlayerBounds'][0], onvalue=True,
+			offvalue=False)
+		selective_checkbox.grid(column=0, row=3, columnspan=2, sticky=tk.W)
+		CreateToolTip(selective_checkbox, 'Only props within the rectangle defined by player 3 and 4 start positions will be moved')
 
 		ttk.Button(button_frame, text='Apply', command=self.apply).grid(column=0, row=0, sticky=(tk.W))
 		ttk.Label(button_frame, textvariable=self.message_var).grid(column=1, row=0, sticky=(tk.W))
@@ -96,6 +109,14 @@ class MoveSublayerAction(Action):
 		rotation = int(0x10000 * self.var('rotation') / 360)
 		repeat = self.var('repeat')
 		copy = self.var('copy')
+		within_player_bounds = self.var('withinPlayerBounds')
+
+		bounds1 = self.map.start_position(None, 3)
+		bounds2 = self.map.start_position(None, 4)
+		min_x = min(bounds1[0], bounds2[0])
+		min_y = min(bounds1[1], bounds2[1])
+		max_x = max(bounds1[0], bounds2[0])
+		max_y = max(bounds1[1], bounds2[1])
 
 		if not copy:
 			repeat = 1
@@ -112,6 +133,11 @@ class MoveSublayerAction(Action):
 
 				if layer == layer1 and (prop.layer_sub == sublayer1 or sublayer1 == -1):
 					layer_sub = prop.layer_sub if sublayer2 == -1 else sublayer2
+
+					if within_player_bounds:
+						if x < min_x or x > max_x or y < min_y or y > max_y:
+							continue
+
 					props_add.append((map, id, x, y, prop, layer2, layer_sub))
 					if not copy:
 						props_del.append((map, id))
